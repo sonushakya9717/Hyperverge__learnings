e29d2b121ac768c574c9d2262d522fe0bdebcfe8
sonushakya9717 <karthik.intern@appscrip.co>
2021-04-18 22:25:52 +0530

    added pair_with_sum_k.py

---
+++ dsa_step_5/pair_with_sum_k.py
@@ -0,0 +1,25 @@
+########### Finding a Pair of Elements with sum K in Array ##########
+
+### Approch-1 ###
+# A=list(map(int,input().split()))
+# k=int(input())
+# flag=False
+# for i in range(len(A)):
+#     flag= False
+#     for j in range(i+1,len(A)):
+#         if A[i]+A[j]==k1
+#             print(A[i],A[j])
+#             flag= True
+#             break
+
+#     if flag== True:
+#         break
+
+### Approch-2 ###
+
+# A=[1,9,7,6,6,5,3]
+# k=int(input())
+# for i in range(len(A)):
+#     if k-A[i] in A:
+#         print(A[i],k-A[i])
+#         break
